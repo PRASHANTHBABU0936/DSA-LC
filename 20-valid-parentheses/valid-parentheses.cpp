13a886782f086a0f205a769ea21e2a5e3ca0e31e
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-01-16 02:16:14 +0530

    Time: 2 ms (15.35%) | Memory: 8.8 MB (86.97%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n= s.length();
+stack <char> st;
+for(int i=0;i<n;i++){
+    if(s[i] == '{' || s[i] == '[' || s[i] == '(')
+    st.push(s[i]);
+    else{
+                if (st.empty()) return false;
+      else if(st.top() == '{'  && s[i] == '}')
+      st.pop();
+       else if(st.top() == '('  && s[i] == ')')
+      st.pop();
+       else if(st.top() == '['  && s[i] == ']')
+      st.pop();
+   else {
+                    return false; // Mismatched bracket
+                } }
+} if(st.empty())
+return true;
+        
+   return false;
+    }
+};
