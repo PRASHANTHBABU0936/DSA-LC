bfca9c9bb7e9fa7c187c93f3abe1576460cce63f
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-01-16 23:31:28 +0530

    Refactor isValid function for parentheses validation
    Refactor isValid function to improve readability and maintainability.

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isValid(string s) {
         int n= s.length();
+        
 stack <char> st;
 for(int i=0;i<n;i++){
     if(s[i] == '{' || s[i] == '[' || s[i] == '(')
@@ -10,12 +11,12 @@ for(int i=0;i<n;i++){
                 if (st.empty()) return false;
       else if(st.top() == '{'  && s[i] == '}')
       st.pop();
-       else if(st.top() == '('  && s[i] == ')')
+          else if(st.top() == '('  && s[i] == ')')
       st.pop();
-       else if(st.top() == '['  && s[i] == ']')
+ else if(st.top() == '['  && s[i] == ']')
       st.pop();
    else {
-                    return false; // Mismatched bracket
+                    return false; 
                 } }
 } if(st.empty())
 return true;
