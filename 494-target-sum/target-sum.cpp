149fbb0d4218a6ea58064b8335729c9dcd534310
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-01-29 17:17:13 +0530

    Time: 929 ms (7.02%) | Memory: 11.5 MB (99.03%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-int solve(int i,int sum,vector <int> &nums,int target){
-    if(i == nums.size()){
-        if(sum == target) return 1;
-        else return 0;
-    }
-    return solve(i + 1, sum + nums[i], nums, target)+solve(i + 1, sum - nums[i], nums, target);
+int hell(int sum,int index,vector<int>& nums,int target){
+if (index == nums.size()) {
+    return (sum == target) ? 1 : 0;
+}
+
+return hell(sum+nums[index],index+1,nums,target)+
+hell(sum-nums[index],index+1,nums,target);
 
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-         return solve(0, 0, nums, target);
+        int sum=0;
+        return hell(0,0,nums,target);
     }
 };
