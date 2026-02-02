c94e1ba38b9b4eb0535e376a62812a941e90f6da
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-02-02 19:19:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (9.38%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,27 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-void hell( vector<int> &res,TreeNode* root){
-    if(root == NULL) return;
-    hell(res,root->left);
-    res.push_back(root->val);
-    hell(res,root->right);
-    // hell(res,root);
-}
     vector<int> inorderTraversal(TreeNode* root) {
-     vector<int> res;
-hell(res,root);
-    return res;
-       
+        vector<int> res;
+        stack<TreeNode*> st;
+
+        TreeNode* hell = root;
+
+        while (hell != nullptr || !st.empty()) {
+
+            while (hell != nullptr) {
+                st.push(hell);
+                hell = hell->left;
+            }
+
+            hell = st.top();
+            st.pop();
+            res.push_back(hell->val);
+
+            hell = hell->right;
+        }
+
+        return res;
     }
 };
