86516c79c8495403d23cbc124139ee28327dc4a2
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-02-13 19:35:36 +0530

    Time: 10 ms (59.20%) | Memory: 25.9 MB (25.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-void hell(int index,vector <int> &nums,int target,int sum,int &c){
- if(index == nums.size()) {
-    if(target == sum) {
-        c++;
+
+vector<vector<int>> dp;
+int offset;
+
+int hell(int index, vector<int> &nums, int target, int sum) {
+
+    if(index == nums.size()) {
+        return (sum == target);
     }
-    return;   // IMPORTANT
+
+    if(dp[index][sum + offset] != -1)
+        return dp[index][sum + offset];
+
+    int takePlus =
+        hell(index + 1, nums, target, sum + nums[index]);
+
+    int takeMinus =
+        hell(index + 1, nums, target, sum - nums[index]);
+
+    return dp[index][sum + offset] = takePlus + takeMinus;
 }
 
+int findTargetSumWays(vector<int>& nums, int target) {
 
-hell(index+1,nums,target,sum+nums[index],c);
-hell(index+1,nums,target,sum-nums[index],c);
+    int total = 0;
+    for(int x : nums) total += x;
 
+    offset = total;
+
+    dp.assign(nums.size(), vector<int>(2 * total + 1, -1));
+
+    return hell(0, nums, target, 0);
 }
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum=0;
-        int c=0;
- hell(0,nums,target,sum,c);
- return c;
-    }
 };
