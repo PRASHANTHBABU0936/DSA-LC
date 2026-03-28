55f258201f4f3fa9d8c557b5b4d446ce96568c4a
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-03-28 23:26:39 +0530

    Time: 10 ms (62.65%) | Memory: 84.8 MB (28.92%) - LeetSync

---
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+
+        for(int i = 0; i < n; i++) {
+            if(lcp[i][i] != n - i) return "";
+        }
+
+        string word(n, '?');
+        char ch = 'a';
+
+        for(int i = 0; i < n; i++) {
+            if(word[i] == '?') {
+                if(ch > 'z') return "";
+                word[i] = ch;
+
+                for(int j = i + 1; j < n; j++) {
+                    if(lcp[i][j] > 0) {
+                        word[j] = ch;
+                    }
+                }
+                ch++;
+            }
+        }
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(word[i] == word[j]) {
+                    dp[i][j] = 1;
+                    if(i + 1 < n && j + 1 < n)
+                        dp[i][j] += dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        if(dp != lcp) return "";
+
+        return word;
+    }
+};
