dda6d23f09184f76f1faccc7590157c1396bc9f5
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-04-16 19:58:40 +0530

    Time: 19 ms (41.98%) | Memory: 13.2 MB (63.37%) - LeetSync

---
+++ 2239-execution-of-all-suffix-instructions-staying-in-a-grid/execution-of-all-suffix-instructions-staying-in-a-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+ int hell(int n,int idx1,int idx2,string &s,int i){
+    int count=0;
+while(i<s.size()){
+    if(s[i] == 'R'){
+idx2++;
+    }
+     else if(s[i] == 'U'){
+        idx1--;
+    }
+      else if(s[i] == 'D'){
+        idx1++;
+    }
+     else if(s[i] == 'L'){
+        idx2--;
+    }
+ 
+            if(idx1 < 0 || idx1 >= n || idx2 < 0 || idx2 >= n)
+                break;
+
+            count++;
+            i++;
+        }
+        return count;
+ }
+    vector<int> executeInstructions(int n, vector<int>& startPos, string s) {
+        // int n = s.size();
+                vector<int> ans(s.size());
+for(int i=0;i<s.size();i++){
+    ans[i]=hell(n,startPos[0],startPos[1],s,i);
+}
+    return ans;}
+};
