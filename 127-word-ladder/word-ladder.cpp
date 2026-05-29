4e3b047631b746e8d290c7be85b8cd4794958691
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-05-29 16:08:48 +0530

    Time: 314 ms (27.59%) | Memory: 19.9 MB (80.69%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,39 +1,47 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-       unordered_set <string> st;
-       for(int i=0;i<wordList.size();i++){
-        st.insert(wordList[i]);
-       }
-               if (st.find(endWord) == st.end()) return 0;
-
-        queue<pair<string,int>> q;
-q.push({beginWord,1});
-while(!q.empty()){
-    string word=q.front().first;
-  int steps = q.front().second;      q.pop();
-                if (word == endWord) return steps;
-
-    for(int j=0;j<word.size();j++){
-char original = word[j];
-       for(char ch = 'a'; ch <= 'z'; ch++){ 
-                        word[j] = ch;
-                    if (st.find(word) != st.end()) {
-                        q.push({word, steps + 1});
-                                                st.erase(word);
-
-
-}
+        
+        set<string> st;
+        st.insert(wordList.begin(), wordList.end());
+        
+        if(st.find(endWord) == st.end()) return 0;
+        
+        queue<string> q;
+        q.push(beginWord);
+        
+        int c = 1;
+        
+        while(!q.empty()){ 
+            int size = q.size();               
+            for(int k = 0; k < size; k++){ 
+                string hell = q.front();
+                q.pop();
+                
+                string original = hell;
+                
+                if(original == endWord) return c;
+                
+                for(int i = 0; i < hell.size(); i++){
+                    char temp = hell[i]; 
+                    
+                    for(char j='a'; j<='z'; j++){
+                        hell[i] = j;
+                        
+                        if(hell != original){
+                            if(st.find(hell) != st.end()){
+                                q.push(hell);
+                                st.erase(hell);
+                            }
+                        }
+                    }
+                    
+                    hell[i] = temp;  
+                }
+            }
+            c++; 
+        }
+        
+        return 0;
     }
-
-                word[j] = original;
-
-}
-// string h=beginWord;
-// for(int j=0;j<h.size();j++){
-//     for()
-// }
-
-    }        return 0;
-}
 };
