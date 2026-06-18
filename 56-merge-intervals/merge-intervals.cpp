dff34edb0e7680e7a113b97c009bdf8693a49250
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-06-18 15:10:19 +0530

    Time: 5 ms (51.65%) | Memory: 24 MB (21.59%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
         sort(intervals.begin(),intervals.end());
-         vector<vector<int>> res;
-         int n=intervals.size();
-         int h=intervals[0][0];
-         int l=intervals[0][1];
-
-         for(int i=1;i<n;i++){
-if(intervals[i][0]<=l && intervals[i][1]>=l){
-l=intervals[i][1];
+         vector<vector<int>>  res;
+bool boolene = false;
+if(n == 1) return intervals;
+         for(int i=1;i<intervals.size();i++){
+if(intervals[i][0] <= intervals[i-1][1]){
+vector<int> crea;
+crea.push_back(intervals[i-1][0]);
+int hell=intervals[i-1][1];
+while(i<n && intervals[i][0] <= hell){
+    hell=max(intervals[i][1],hell);
+i++;
 }
-else if(intervals[i][0]<=l && intervals[i][1]<=l){
-    continue;
-}
-else {
-    res.push_back({h,l});
-    h=intervals[i][0];
-    l=intervals[i][1];
+crea.push_back(hell);
+res.push_back(crea);
+i--;}
+else{
+        // if((i == n-1)) boolene=true;
+     if(res.empty() || res.back()[1] < intervals[i-1][0]) {
+                    res.push_back(intervals[i-1]);
+                }
 }
          }
-         res.push_back({h,l});
-    return res;}
+        if(res.empty() || res.back()[1] < intervals[n-1][0]) {
+            res.push_back(intervals[n-1]);}
+        return res;}
 };
