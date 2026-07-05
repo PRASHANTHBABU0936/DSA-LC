18900e56a8789fac20d3adcfc700541ed4c16f5d
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-07-05 23:50:51 +0530

    Time: 48 ms (8.63%) | Memory: 27.7 MB (15.69%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
- int dp[1001][1001];
-int c=0;
-int hell(int idx1,int idx2,string &text1,string &text2){
-    if(idx1 == text1.size() || idx2 == text2.size()){
-        return 0;}
-         if(dp[idx1][idx2] != -1) return dp[idx1][idx2]; 
-    int g=0;
-if(text1[idx1] == text2[idx2]){
-   return dp[idx1][idx2] = 1+hell(idx1+1,idx2+1,text1,text2);}
-   
-        return dp[idx1][idx2]= max(hell(idx1+1, idx2, text1, text2),hell(idx1, idx2+1, text1, text2));
-    }
-    // return g;
+vector<vector<int>> dp;
+int hell(string &text1, string &text2,int n,int m,int index1,int index2){
+if(index1 > n-1 || index2 > m-1) return 0;
+if(dp[index1][index2] != -1) return dp[index1][index2];
+else if(text1[index1] == text2[index2]) return dp[index1][index2]=1+hell(text1,text2,n,m,index1+1,index2+1);
 
+else return dp[index1][index2]=max(hell(text1,text2,n,m,index1+1,index2),hell(text1,text2,n,m,index1,index2+1));
+
+return dp[index1][index2];}
     int longestCommonSubsequence(string text1, string text2) {
-    //   int c=0;
-       memset(dp, -1, sizeof(dp));
-    return  hell(0,0,text1,text2);}
+      int n=text1.size();
+      int m = text2.size();
+dp.assign(n, vector<int>(m, -1));       return  hell(text1,text2,n,m,0,0);
+    }
 };
