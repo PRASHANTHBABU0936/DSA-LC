120ace4346e39c7cde2b406f9beaeb49f07a6eda
Prashanth Babu <153594353+PRASHANTHBABU0936@users.noreply.github.com>
2026-07-10 14:12:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (25.67%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-vector<vector<int>> memo;
-int hell(int index2,vector<vector<int>>& triangle,int index1,int n){
-if(index1 >= n || index2 >= triangle[index1].size()) return 0;
+vector<vector<int>>  dp;
 
- if(memo[index1][index2] != INT_MAX) return memo[index1][index2];
+// int hell(int index2,vector<vector<int>>& triangle,int index1,int n){
+// if(index1 >= n || index2 >= triangle[index1].size()) return 0;
 
-return memo[index1][index2]=triangle[index1][index2]+min(hell(index2+1,triangle,index1+1,n),hell(index2,triangle,index1+1,n));}
+//  if(memo[index1][index2] != INT_MAX) return memo[index1][index2];
+
+// return memo[index1][index2]=triangle[index1][index2]+min(hell(index2+1,triangle,index1+1,n),hell(index2,triangle,index1+1,n));}
+
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         int m = triangle[0].size();
+        
+
+        dp.resize(n + 1, vector<int>(n + 1, 0));
         // memo.resize(n,vector<int>(m,-1));
-        memo.resize(n);
-        for(int i=0;i<n;i++){
-            memo[i].resize(triangle[i].size(), INT_MAX);
+        // memo.resize(n);
+        for(int i=n-1;i>=0;i--){
+            for (int j = 0; j < triangle[i].size(); j++) {
+dp[i][j]=triangle[i][j]+min(dp[i+1][j+1],dp[i+1][j]);
+            }
+            // memo[i].resize(triangle[i].size(), INT_MAX);
         }
-        return hell(0,triangle,0,n);
-    }
+        // return hell(0,triangle,0,n);
+    return dp[0][0];}
 };
